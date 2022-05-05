a2d31fcb4eb7d2c27904f972a965faa3d8448780
filip <filip@FILIPE>
2022-05-05 14:29:05 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto1/Caixa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto1
+{
+    internal class Caixa
+    {
+        public string _name;
+        public double _number;
+        public bool _deposit;
+        public double _money;
+        public double _balance;
+
+        public Caixa()
+        {
+
+        }
+
+        public Caixa(string nome, double numero, bool deposito, double dinheiro, double saldo)
+        {
+            _name = nome; _number = numero; _deposit = deposito; _money = dinheiro; _balance = saldo;
+        }
+
+        public void Somar(double dinheiro)
+        {
+            _money += dinheiro;
+        }
+
+        public void Sacar(double dinheiro)
+        {
+            _money -= dinheiro - 5.0;
+        }
+
+        public double SaldoFinal()
+        {
+            return _money;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta: " + _number + "," + "Titular: " + _name + "," + "Saldo: " + SaldoFinal().
+            ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
---
+++ Projeto1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto1
+{
+
+    class program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Caixa x = new Caixa();
+
+
+            Console.Write("Número da conta: ");
+            x._number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Titular da conta: ");
+            x._name = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? ");
+            x._deposit = bool.Parse(Console.ReadLine());
+
+
+
+            if (x._deposit == false)
+            { Console.WriteLine("Dados da conta: " + x); }
+
+            else
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+
+                double depositar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.Somar(depositar);
+                Console.WriteLine("Dados da conta: " + x);
+                Console.Write("Deposite um valor: ");
+                depositar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.Somar(depositar);
+                Console.WriteLine("Dados atualizados: " + x);
+                Console.Write("Entre com um valor para sacar: ");
+                double tirar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.Sacar(tirar);
+                Console.WriteLine("Dados atualizados: " + x);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
